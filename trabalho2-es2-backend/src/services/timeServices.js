358f80286e4872c4f019e32a4e301852133240ed
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-03-30 04:02:51 -0300

    fix: corrige rota de listagem de times

--- trabalho2-es2-backend/src/services/timeServices.js
+++ trabalho2-es2-backend/src/services/timeServices.js
@@ -2,7 +2,7 @@ const database = require('../database/dbConfig');
 
 const listarTimes = () => {
     return new Promise((aceito, rejeitado) => {
-        database.query('SELECT * FROM time WHERE isAtivo = 1', (error, results) =>{
+        database.query('SELECT * FROM time', (error, results) =>{
             if (error) { rejeitado(error); return; }
             aceito(results);
         });
